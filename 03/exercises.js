c0b928e0ea051aff6c527a5b629dd70279637d00
Taylor Huston <taylor.a.huston@gmail.com>
2016-03-23 22:01:21 -0700

    Implement isEven exerise

--- 03/exercises.js
+++ 03/exercises.js
@@ -1,4 +1,4 @@
-function min(x,y) {
+function min(x, y) {
   if (x <= y) {
     return x
   } else {
@@ -6,4 +6,17 @@ function min(x,y) {
   }
 }
 
-console.log(min(10,7));
+function isEven(x) {
+  if (x == 0 || x == 1) {
+    return x;
+  } else if (x > 1) {
+    return isEven(x - 2);
+  } else {
+    return isEven(x + 2);
+  }
+}
+
+console.log(min(10, 7));
+console.log(isEven(50));
+console.log(isEven(75))
+console.log(isEven(-75))
