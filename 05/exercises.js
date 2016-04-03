1a1cf7817badace7fa7f3cc7a383885f7912e9de
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-02 20:22:19 -0700

    Implement finding the average mother's age when the child was born

--- 05/exercises.js
+++ 05/exercises.js
@@ -1,3 +1,7 @@
+var data = require('./ancestry.js');
+var ancestry = JSON.parse(data);
+
+/**Flatten an array of arrays into a single array**/
 function flattening(arrays) {
   var newArray = arrays.reduce(function(x, y) {
     return x.concat(y);
@@ -5,4 +9,28 @@ function flattening(arrays) {
   return newArray;
 };
 
+/**Calculate the average age difference between mothers and children**/
+var byName = {}
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+function average(array) {
+  function plus(x,y) {
+    return (x+y);
+  }
+  return array.reduce(plus) /array.length;
+}
+
+function averageAge() {
+  var ages = [];
+  ancestry.forEach(function(person) {
+    if (byName[person.mother] != null) {
+      var mom = byName[person.mother]
+      ages.push(person.born - mom.born);
+    }
+  })
+  return ages;
+}
 console.log(flattening([[1, 2, 3], [4, 5], [6]]));
+console.log(average(averageAge()));
