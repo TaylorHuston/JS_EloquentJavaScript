5d1013dfb8caa5f83b888ff6ed17dc8b94715033
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-02 20:57:32 -0700

    Calculate the average age by century

--- 05/exercises.js
+++ 05/exercises.js
@@ -1,30 +1,33 @@
 var data = require('./ancestry.js');
 var ancestry = JSON.parse(data);
 
+var byName = {}
+ancestry.forEach(function (person) {
+  byName[person.name] = person;
+});
+
+function average(array) {
+  function plus(x, y) {
+    return (x + y);
+  }
+  return array.reduce(plus) / array.length;
+}
+
+
 /**Flatten an array of arrays into a single array**/
 function flattening(arrays) {
-  var newArray = arrays.reduce(function(x, y) {
+  var newArray = arrays.reduce(function (x, y) {
     return x.concat(y);
   }, []);
   return newArray;
 };
 
+console.log(flattening([[1, 2, 3], [4, 5], [6]]));
+
 /**Calculate the average age difference between mothers and children**/
-var byName = {}
-ancestry.forEach(function(person) {
-  byName[person.name] = person;
-});
-
-function average(array) {
-  function plus(x,y) {
-    return (x+y);
-  }
-  return array.reduce(plus) /array.length;
-}
-
 function averageAge() {
   var ages = [];
-  ancestry.forEach(function(person) {
+  ancestry.forEach(function (person) {
     if (byName[person.mother] != null) {
       var mom = byName[person.mother]
       ages.push(person.born - mom.born);
@@ -32,5 +35,31 @@ function averageAge() {
   })
   return ages;
 }
-console.log(flattening([[1, 2, 3], [4, 5], [6]]));
+
 console.log(average(averageAge()));
+
+/**Historical Life Expectancy**/
+
+function GroupAges() {
+  var ageHash = {}
+  ancestry.forEach(function (person) {
+    var key = (Math.ceil(person.died / 100));
+    var age = person.died - person.born;
+    if (key in ageHash) {
+      ageHash[key].push(age);
+    } else {
+      ageHash[key] = [age];
+    }
+
+  })
+  return ageHash;
+}
+
+function CalcAverage(ages) {
+  var averageAges = {}
+  for (century in ages) {
+    averageAges[century] = average(ages[century]);
+  }
+  return averageAges
+}
+console.log(CalcAverage(GroupAges()));
