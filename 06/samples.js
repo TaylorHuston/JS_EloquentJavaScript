ad8ba027d5d288031b1fd0ce77edcb8ebf1bb7b0
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-03 22:07:02 -0700

    Apply and call examples

--- 06/samples.js
+++ 06/samples.js
@@ -1,7 +1,33 @@
 var rabbit = {};
 
-rabbit.speak = function(line) {
-  console.log("The rabbit says '" + line +"'");
+rabbit.speak = function (line) {
+  console.log("The rabbit says '" + line + "'");
 }
 
 rabbit.speak("I'm alive");
+
+
+
+function speak(line) {
+  console.log("The " + this.type + " rabbit says '" +
+    line + "'");
+}
+var whiteRabbit = {
+  type: "white",
+  speak: speak
+};
+var fatRabbit = {
+  type: "fat",
+  speak: speak
+};
+
+whiteRabbit.speak("Hi");
+fatRabbit.speak("Bye");
+
+speak.apply(fatRabbit, ["Nom nom"]);
+speak.call({type: "old"}, "Oh Myyyyyyyy.");
+
+//Prototypes
+console.log(Object.getPrototypeOf({}) == Object.prototype);
+console.log(Object.getPrototypeOf(isNaN) == Function.prototype);
+console.log(Object.getPrototypeOf([]) == Array.prototype);
