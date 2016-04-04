3bc1a24c18cd36f6964e7fc3b5c2a9ad3062d8f9
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-03 22:11:56 -0700

    Constructor example

--- 06/samples.js
+++ 06/samples.js
@@ -31,3 +31,23 @@ speak.call({type: "old"}, "Oh Myyyyyyyy.");
 console.log(Object.getPrototypeOf({}) == Object.prototype);
 console.log(Object.getPrototypeOf(isNaN) == Function.prototype);
 console.log(Object.getPrototypeOf([]) == Array.prototype);
+
+//Prototype (like class?)
+var protoRabbit = {
+  speak: function(line) {
+    console.log("The " + this.type + " rabbit says '" +
+                line + "'");
+  }
+};
+var killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEEE!");
+
+//Constructor
+function Rabbit(type) {
+  this.type = type;
+}
+
+var killerRabbit = new Rabbit("killer");
+var blackRabbit = new Rabbit("black");
+console.log(blackRabbit.type);
