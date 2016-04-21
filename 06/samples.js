e3de8ba972a9487881503c563cedfdbf9e1e987e
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-20 23:16:38 -0700

    Finish reading Chapter 6

--- 06/samples.js
+++ 06/samples.js
@@ -72,4 +72,18 @@ console.log(Rabbit.prototype.teeth);
 //Create an object without a prototype
 var map = Object.create(null);
 map["someProp"] = 0.069;
-console.log("toString" in map);  //False.
+console.log("toString" in map);  //False
+
+
+var pile = {
+  elements: ["eggshell", "orange peel", "worm"],
+  get height() {
+    return this.elements.length;
+  },
+  set height(value) {
+    console.log("Ignoring attempt to set height to", value);
+  }
+};
+
+console.log(pile.height); //3
+pile.height = 100; //Ignoring attempt to set height to 100
--- 06/tabler.js
+++ 06/tabler.js
@@ -88,6 +88,25 @@ UnderlinedCell.prototype.draw = function (width, height) {
     .concat([repeat("-", width)]);
 };
 
+Object.defineProperty(TextCell.prototype, "heightProp", {
+  get: function () {
+    return this.text.length;
+  }
+});
+
+function RTextCell(text) {
+  TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype);
+RTextCell.prototype.draw = function (width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(repeat(" ", width - line.length) + line);
+  }
+  return result;
+};
+
 
 //Sample checkboard grid
 var rows = [];
@@ -106,14 +125,21 @@ console.log(drawTable(rows));
 //Information from MOUNTAINS file
 function dataTable(data) {
   var keys = Object.keys(data[0]);
-  var headers = keys.map(function(name) {
+  var headers = keys.map(function (name) {
     return new UnderlinedCell(new TextCell(name));
   });
-  var body = data.map(function(row) {
-    return keys.map(function(name) {
-      return new TextCell(String(row[name]));
+  var body = data.map(function (row) {
+    return keys.map(function (name) {
+      var value = row[name];
+      // This was changed:
+      if (typeof value == "number")
+        return new RTextCell(String(value));
+      else
+        return new TextCell(String(value));
     });
   });
   return [headers].concat(body);
 }
 console.log(drawTable(dataTable(DATA)));
+
+
