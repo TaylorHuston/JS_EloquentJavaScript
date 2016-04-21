13a9a1547c834237ccdfa9636b0709ff49cd9d22
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-20 23:46:02 -0700

    Grid to contain the world logic

--- 07/life.js
+++ 07/life.js
@@ -20,3 +20,26 @@ function Vector(x, y) {
 Vector.prototype.plus = function(other) {
   return new Vector(this.x + other.x, this.y + other.y);
 };
+
+
+//Grid for the logic of the world
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+Grid.prototype.isInside = function(vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+         vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function(vector) {
+  return this.space[vector.x + this.width * vector.y];
+};
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+};
+
+var grid = new Grid(5, 5);
+console.log(grid.get(new Vector(1, 1))); // undefined
+grid.set(new Vector(1, 1), "X");
+console.log(grid.get(new Vector(1, 1))); // X
