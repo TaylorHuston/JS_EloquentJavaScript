cb6ca7201567475e6644bf3a28a9607b98d6ee96
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-21 23:43:35 -0700

    Add randomElement helper function, refactor code

--- 07/life.js
+++ 07/life.js
@@ -1,3 +1,12 @@
+//HELPER FUNCTIONS
+//Helper function to grab a random element out of a given array
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
+//WORLD LOGIC
+
 //The 'world'. # are walls and rocks, 0 are critters"
 var plan = ["############################",
             "#      #    #      o      ##",
@@ -44,6 +53,8 @@ console.log(grid.get(new Vector(1, 1))); // undefined
 grid.set(new Vector(1, 1), "X");
 console.log(grid.get(new Vector(1, 1))); // X
 
+//CRITTER LOGIC
+
 //Directions that the critters can go in
 var directions = {
   "n":  new Vector( 0, -1),
@@ -55,3 +66,5 @@ var directions = {
   "w":  new Vector(-1,  0),
   "nw": new Vector(-1, -1)
 };
+
+
