592d61ceeb943552be317d8ccf9c491e72dc72ec
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-21 23:45:26 -0700

    Implement basic critter

--- 07/life.js
+++ 07/life.js
@@ -26,7 +26,7 @@ function Vector(x, y) {
   this.x = x;
   this.y = y;
 }
-Vector.prototype.plus = function(other) {
+Vector.prototype.plus = function (other) {
   return new Vector(this.x + other.x, this.y + other.y);
 };
 
@@ -37,14 +37,14 @@ function Grid(width, height) {
   this.width = width;
   this.height = height;
 }
-Grid.prototype.isInside = function(vector) {
+Grid.prototype.isInside = function (vector) {
   return vector.x >= 0 && vector.x < this.width &&
-         vector.y >= 0 && vector.y < this.height;
+    vector.y >= 0 && vector.y < this.height;
 };
-Grid.prototype.get = function(vector) {
+Grid.prototype.get = function (vector) {
   return this.space[vector.x + this.width * vector.y];
 };
-Grid.prototype.set = function(vector, value) {
+Grid.prototype.set = function (vector, value) {
   this.space[vector.x + this.width * vector.y] = value;
 };
 
@@ -53,18 +53,34 @@ console.log(grid.get(new Vector(1, 1))); // undefined
 grid.set(new Vector(1, 1), "X");
 console.log(grid.get(new Vector(1, 1))); // X
 
+
 //CRITTER LOGIC
 
 //Directions that the critters can go in
 var directions = {
-  "n":  new Vector( 0, -1),
-  "ne": new Vector( 1, -1),
-  "e":  new Vector( 1,  0),
-  "se": new Vector( 1,  1),
-  "s":  new Vector( 0,  1),
-  "sw": new Vector(-1,  1),
-  "w":  new Vector(-1,  0),
+  "n": new Vector(0, -1),
+  "ne": new Vector(1, -1),
+  "e": new Vector(1, 0),
+  "se": new Vector(1, 1),
+  "s": new Vector(0, 1),
+  "sw": new Vector(-1, 1),
+  "w": new Vector(-1, 0),
   "nw": new Vector(-1, -1)
 };
 
+var directionNames = "n ne e se s sw w nw".split(" ");
 
+
+//Basic critter that just moves in a straight line until it hits a wall
+function BouncingCritter() {
+  this.direction = randomElement(directionNames);
+};
+
+BouncingCritter.prototype.act = function (view) {
+  if (view.look(this.direction) != " ")
+    this.direction = view.find(" ") || "s";
+  return {
+    type: "move",
+    direction: this.direction
+  };
+};
